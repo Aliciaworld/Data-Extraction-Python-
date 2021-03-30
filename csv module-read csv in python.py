31eb6a4c052f6870cd99f4c0289ad5b2678d0987
Alicia <xiaojingworld@gmail.com>
2021-03-30 06:23:28 -0400

    Add files via upload

---
+++ csv module-read csv in python.py
@@ -0,0 +1,9 @@
+import csv
+
+def parse_csv(filename):
+    data = []
+    with open(filename, 'rb') as f:
+        entry = csv.DictReader(f)
+        for line in entry:
+            data.append(line)
+    return data
